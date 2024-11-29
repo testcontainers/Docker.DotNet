c877a5db3d9e9ca0ea3934fa7bc78b11a9e9b644
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2024-11-29 14:15:29 +0100

    fix: Use net9.0 to build projects

--- src/Docker.DotNet/Properties/InternalVisibleTo.cs
+++ src/Docker.DotNet/Properties/InternalVisibleTo.cs
@@ -1,5 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Docker.DotNet.JsonSerializer8.Tests" + StrongNamePublicKeys.DockerDotNetPublicKey)]
-[assembly: InternalsVisibleTo("Docker.DotNet.JsonSerializer9.Tests" + StrongNamePublicKeys.DockerDotNetPublicKey)]
 [assembly: InternalsVisibleTo("Docker.DotNet.Tests" + StrongNamePublicKeys.DockerDotNetPublicKey)]
