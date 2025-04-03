7394389785c622ae0d18ff5ed60d6df781cdf477
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-04-03 09:08:49 +0200

    chore: Remove information that this project runs under the .NET Foundation

--- src/Docker.DotNet/Microsoft.Net.Http.Client/UnixDomainSocketEndPoint.cs
+++ src/Docker.DotNet/Microsoft.Net.Http.Client/UnixDomainSocketEndPoint.cs
@@ -1,7 +1,3 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE-MIT.txt file for more information.
-
 namespace Microsoft.Net.Http.Client;
 
 internal sealed class UnixDomainSocketEndPoint : EndPoint
