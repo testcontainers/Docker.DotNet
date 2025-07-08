6fc513c7eeaa885947083682f753234db72c09f6
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-07-08 16:47:11 +0200

    fix: Set auth header in basic auth handler implementation (#29)

--- src/Docker.DotNet.BasicAuth/BasicAuthHandler.cs
+++ src/Docker.DotNet.BasicAuth/BasicAuthHandler.cs
@@ -6,11 +6,27 @@ internal class BasicAuthHandler : DelegatingHandler
 
     private readonly MaybeSecureString _password;
 
+    private readonly Lazy<AuthenticationHeaderValue> _authHeader;
+
     public BasicAuthHandler(MaybeSecureString username, MaybeSecureString password, HttpMessageHandler httpMessageHandler)
         : base(httpMessageHandler)
     {
         _username = username.Copy();
         _password = password.Copy();
+
+        _authHeader = new Lazy<AuthenticationHeaderValue>(() =>
+        {
+            var credentials = $"{_username}:{_password}";
+            var bytes = Encoding.ASCII.GetBytes(credentials);
+            var base64 = Convert.ToBase64String(bytes);
+            return new AuthenticationHeaderValue("Basic", base64);
+        });
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.Headers.Authorization = _authHeader.Value;
+        return base.SendAsync(request, cancellationToken);
     }
 
     protected override void Dispose(bool disposing)
