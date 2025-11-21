b69450f25fa8c0ba6b7abd8e207167a589dcfdfd
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-11-21 20:34:45 +0100

    fix: Adjust Base64 converter

--- src/Docker.DotNet/Base64Converter.cs
+++ src/Docker.DotNet/Base64Converter.cs
@@ -4,14 +4,13 @@ internal class Base64Converter : JsonConverter<IList<byte>>
 {
     public override IList<byte> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var base64 = reader.GetString();
-        return base64 == null ? null : Encoding.UTF8.GetBytes(base64);
+        var base64String = reader.GetString();
+        return base64String == null ? null : Convert.FromBase64String(base64String);
     }
 
     public override void Write(Utf8JsonWriter writer, IList<byte> value, JsonSerializerOptions options)
     {
-        // TODO: This seams not correct, it breaks: 'SwarmConfig_CanCreateAndRead'.
-        var base64 = Encoding.UTF8.GetString(value.ToArray());
-        writer.WriteStringValue(base64);
+        var base64String = Convert.ToBase64String(value.ToArray());
+        writer.WriteStringValue(base64String);
     }
 }
