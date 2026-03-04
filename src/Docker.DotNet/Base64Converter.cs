a0acf6a6705c5ae9743924486b2dc3e6171995d5
campersau <buchholz.bastian@googlemail.com>
2026-03-04 13:46:04 +0100

    chore: Use GetBytesFromBase64 / WriteBase64StringValue to avoid string allocations (#80)

--- src/Docker.DotNet/Base64Converter.cs
+++ src/Docker.DotNet/Base64Converter.cs
@@ -4,13 +4,11 @@ internal class Base64Converter : JsonConverter<IList<byte>>
 {
     public override IList<byte> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var base64String = reader.GetString();
-        return base64String == null ? null : Convert.FromBase64String(base64String);
+        return reader.GetBytesFromBase64();
     }
 
     public override void Write(Utf8JsonWriter writer, IList<byte> value, JsonSerializerOptions options)
     {
-        var base64String = Convert.ToBase64String(value.ToArray());
-        writer.WriteStringValue(base64String);
+        writer.WriteBase64StringValue(value.ToArray());
     }
 }
