3ffe7a5d385f8abfc9a73ae3fef64e572263d645
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2026-03-04 13:37:47 +0100

    chore: Simplify integration of the library

--- src/Docker.DotNet.X509/CertificateCredentials.cs
+++ src/Docker.DotNet.X509/CertificateCredentials.cs
@@ -18,30 +18,37 @@ public class CertificateCredentials : IAuthProvider
 #if NET6_0_OR_GREATER
         if (handler is SocketsHttpHandler socketsHandler)
         {
-            socketsHandler.SslOptions.ClientCertificates = new X509Certificate2Collection();
-            socketsHandler.SslOptions.ClientCertificates.Add(_certificate);
+            if (_certificate != null)
+            {
+                socketsHandler.SslOptions.ClientCertificates = new X509Certificate2Collection();
+                socketsHandler.SslOptions.ClientCertificates.Add(_certificate);
+            }
+
             socketsHandler.SslOptions.RemoteCertificateValidationCallback = ServerCertificateValidationCallback;
             return socketsHandler;
         }
 #else
         if (handler is HttpClientHandler httpHandler)
         {
-            httpHandler.ClientCertificates.Add(_certificate);
+            if  (_certificate != null && !httpHandler.ClientCertificates.Contains(_certificate))
+            {
+                httpHandler.ClientCertificates.Add(_certificate);
+            }
+
             httpHandler.ServerCertificateCustomValidationCallback = (message, certificate, chain, sslPolicyErrors) => ServerCertificateValidationCallback(message, certificate, chain, sslPolicyErrors);
             return httpHandler;
         }
 #endif
-        if (handler is not ManagedHandler managedHandler)
+        if (handler is ManagedHandler managedHandler)
         {
-            return handler;
-        }
+            if  (_certificate != null && !managedHandler.ClientCertificates.Contains(_certificate))
+            {
+                managedHandler.ClientCertificates.Add(_certificate);
+            }
 
-        if (!managedHandler.ClientCertificates.Contains(_certificate))
-        {
-            managedHandler.ClientCertificates.Add(_certificate);
+            managedHandler.ServerCertificateValidationCallback = ServerCertificateValidationCallback;
+            return managedHandler;
         }
-
-        managedHandler.ServerCertificateValidationCallback = ServerCertificateValidationCallback;
 
         return handler;
     }
