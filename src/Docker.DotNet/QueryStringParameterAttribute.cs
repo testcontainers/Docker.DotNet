b9fa870fedf8dab7867e118c556af87cc3281d2c
campersau <buchholz.bastian@googlemail.com>
2026-04-01 12:00:34 +0200

    remove ConcurrentDictionary and use static field instead for query string converter

--- src/Docker.DotNet/QueryStringParameterAttribute.cs
+++ src/Docker.DotNet/QueryStringParameterAttribute.cs
@@ -23,7 +23,7 @@ internal class QueryStringParameterAttribute : Attribute
 
 internal sealed class QueryStringParameterAttribute<TConverter>(string name, bool required) : QueryStringParameterAttribute(name, required) where TConverter : IQueryStringConverter, new()
 {
-    private static readonly ConcurrentDictionary<Type, IQueryStringConverter> ConverterInstanceRegistry = new ConcurrentDictionary<Type, IQueryStringConverter>();
+    private static TConverter? ConverterInstance;
 
-    public override IQueryStringConverter GetConverter() => ConverterInstanceRegistry.GetOrAdd(typeof(TConverter), static _ => new TConverter());
+    public override IQueryStringConverter GetConverter() => ConverterInstance ??= new TConverter();
 }
