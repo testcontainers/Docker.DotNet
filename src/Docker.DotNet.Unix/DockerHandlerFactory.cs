06719f77bf94204f9445c980abda9290b0072fed
campersau <buchholz.bastian@googlemail.com>
2026-05-02 08:47:44 +0200

    feat(unix): Support cancellation token in connect on .NET (#103)

--- src/Docker.DotNet.Unix/DockerHandlerFactory.cs
+++ src/Docker.DotNet.Unix/DockerHandlerFactory.cs
@@ -26,14 +26,19 @@ public sealed class DockerHandlerFactory : IDockerHandlerFactory<UnixSocketTrans
         var socketPath = uri.LocalPath;
         uri = new UriBuilder(Uri.UriSchemeHttp, socketName).Uri;
 
-        var socketOpener = new ManagedHandler.SocketOpener(async (_, _, _) =>
+        var socketOpener = new ManagedHandler.SocketOpener(async (_, _, cancellationToken) =>
         {
             var endpoint = new UnixDomainSocketEndPoint(socketPath);
 
             var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
 
+#if NET
+            await socket.ConnectAsync(endpoint, cancellationToken)
+                .ConfigureAwait(false);
+#else
             await socket.ConnectAsync(endpoint)
                 .ConfigureAwait(false);
+#endif
 
             return socket;
         });
